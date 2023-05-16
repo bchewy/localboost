a46916131123efce54c22163843fbe65bb5cb65b
Brian Chew <brian@bchewy.com>
2023-05-16 21:28:29 +0800

    .gitignore

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1TAYeKq8j8o3IEJNf-RSDAhgd-ooSLLs",
-  authDomain: "localboost-f9623.firebaseapp.com",
-  projectId: "localboost-f9623",
-  storageBucket: "localboost-f9623.appspot.com",
-  messagingSenderId: "280142981800",
-  appId: "1:280142981800:web:14f14b0e634a7a9b0ef09d",
-  measurementId: "G-32KDLSJJDK"
+        apiKey: process.env.REACT_APP_API_KEY,
+        authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_APP_ID,
+        measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+        databaseURL: process.env.REACT_APP_DATABASEURL,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 
-export default firebase;
+export default database;
