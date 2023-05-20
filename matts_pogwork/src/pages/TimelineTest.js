59deeed03c60d44c78d7839d4bf805b023b5535b
tanhaoen <nathanieltan1701@gmail.com>
2023-05-21 04:03:21 +0800

    Added functionality to timeline

--- matts_pogwork/src/pages/TimelineTest.js
+++ matts_pogwork/src/pages/TimelineTest.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Grid, Button } from '@mui/material';
+
 import { useState, useEffect } from "react";
 import "../style.css";
 import { Chrono } from "react-chrono";
@@ -22,47 +24,85 @@ const TimelineTest = (props) => {
     const q = query(
       collection(docRef, "milestones"),
       where("milestone_number", "==", currentMilestone),
-      orderBy("timestamp", "asc")
+      orderBy("timestamp", "desc")
     );
     const querySnapshot = await getDocs(q);
-    const newItems = querySnapshot.docs.map(item => ({
-      title: item.data().timestamp.toDate().toLocaleString(),
-      cardTitle: item.data().subject,
-      cardDetailedText: "something"
-    }));
+
+    const newItems = [];
+    querySnapshot.forEach(item => {
+      const urlArray = item.data().upload_url;
+      const description = item.data().description;
+
+      const Uploads = ({ urlArray }) => {
+        if (urlArray === undefined) return null;
+        return (
+        <div>
+          <p><b>Uploads</b></p>
+          {urlArray.map((url, index) => (
+            <p style={{align: "left"}} key={index}>
+              <a href={url}>{url}</a>
+            </p>
+          ))}
+        </div>
+        );
+      };
+
+      const DownloadButton = () => {
+
+        const handleDownload = () => {
+          urlArray.forEach((url) => {
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = url;
+            link.click();
+          });
+        };
+
+        return (
+          <Button size="small" variant="contained" onClick={handleDownload} color='primary'>
+            Download
+          </Button>
+        );
+      };
+
+      const Content = () => {
+        return (
+          <Grid container spacing={6} width="1000px" justifyContent="center" alignItems="flex-start">
+            <Grid item width="100px" sx={{ mr:5 }}>
+              <DownloadButton />
+            </Grid>
+            <Grid item width="400px" zeroMinWidth>
+              <p>{description}</p>
+            </Grid>
+          </Grid>
+        );
+      };
+
+      newItems.push({
+        title: item.data().timestamp.toDate().toLocaleString(),
+        cardTitle: item.data().subject,
+        // timelineContent: <div>
+        //   <p>Uploads</p>
+        //   <hr></hr>
+        //   {urlArray.map(url => <p><a href={url}>{url}</a></p>)}</div>,
+        timelineContent: <Content />
+        
+      });
+    
     setItems(newItems);
+    });
   };
   
   useEffect(() => {
     fetchData();
-  }, [projectID, currentMilestone]);
-  // OLD METHOD, TIMESTAMP TYPE NEEDS TO BE SAME AS ABOVE
-  // const docRef = doc(db, "projects", projectID);
+  }, []);
 
-  // const q = query(
-  //   collection(docRef, "milestones"),
-  //   where("milestone_number", "==", currentMilestone),
-  //   orderBy("timestamp", "asc"));
-
-  // const getChangelog = async () => {
-  //   const querySnapshot = await getDocs(q);
-  //   querySnapshot.forEach(item => {
-  //     setItems(items => [...items, {
-  //       title: item.data().timestamp.,
-  //       cardTitle: item.data().subject,
-  //       cardDetailedText: "something"}]);
-  //   });
-  // };
-
-
-
-
-
-  console.log('items arr:', items);
+  console.log(items.length);
 
   return (
-    <div style={{ width: '500px', height: '950px' }}>
-      {items.length > 0 && <Chrono items={items} mode="VERTICAL" />}
+    <div style={{ width: '1200px', height: '800px' }}>
+      <h1>Project {projectID}: Milestone {currentMilestone}</h1>
+      {items.length > 0 && <Chrono items={items} mode="VERTICAL" cardHeight="100" cardWidth="800" />}
     </div>
   )
 }
