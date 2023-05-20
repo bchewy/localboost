94e07fb7c05f1fd769561380ee8a7869c16cf9a8
tanhaoen <nathanieltan1701@gmail.com>
2023-05-20 21:14:22 +0800

    Playing around with timeline

--- matts_pogwork/src/pages/firebase-store.js
+++ matts_pogwork/src/pages/firebase-store.js
@@ -11,6 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+// const app = firebase.initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
