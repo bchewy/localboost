6c89fdf9630fac25c2ef8ea11c2e3416bf1c3482
tanhaoen <nathanieltan1701@gmail.com>
2023-05-21 20:16:04 +0800

    Working for timeline

--- matts_pogwork/src/pages/TimelineTest.js
+++ matts_pogwork/src/pages/TimelineTest.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Grid, Button } from '@mui/material';
+import { Grid, Button, TextField } from '@mui/material';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "../style.css";
+import "./styles.css";
 import { Chrono } from "react-chrono";
 
 import { app } from './firebase';
-import { getFirestore, collection, getDocs, query, serverTimestamp, onSnapshot, orderBy, doc, where }
+import { getFirestore, collection, getDocs, query, serverTimestamp, addDoc, orderBy, doc, where, onSnapshot }
   from "firebase/firestore";
+
+import DownloadButton from "../components/FileDownloadButton";
 
 const db = getFirestore(app);
 
@@ -19,7 +22,7 @@ const TimelineTest = (props) => {
 
   const [items, setItems] = useState([]);
 
-  const fetchData = async () => {
+  const fetchCardData = async () => {
     const docRef = doc(db, "projects", projectID);
     const q = query(
       collection(docRef, "milestones"),
@@ -33,75 +36,161 @@ const TimelineTest = (props) => {
       const urlArray = item.data().upload_url;
       const description = item.data().description;
 
-      const Uploads = ({ urlArray }) => {
-        if (urlArray === undefined) return null;
-        return (
-        <div>
-          <p><b>Uploads</b></p>
-          {urlArray.map((url, index) => (
-            <p style={{align: "left"}} key={index}>
-              <a href={url}>{url}</a>
-            </p>
-          ))}
-        </div>
-        );
-      };
+      // const Uploads = ({ urlArray }) => {
+      //   if (urlArray === undefined) return null;
+      //   return (
+      //   <div>
+      //     <p><b>Uploads</b></p>
+      //     {urlArray.map((url, index) => (
+      //       <p style={{align: "left"}} key={index}>
+      //         <a href={url}>{url}</a>
+      //       </p>
+      //     ))}
+      //   </div>
+      //   );
+      // };
 
-      const DownloadButton = () => {
+      const Content = () => {
+        const [comment, setComment] = useState("");
 
-        const handleDownload = () => {
-          urlArray.forEach((url) => {
-            const link = document.createElement("a");
-            link.href = url;
-            link.download = url;
-            link.click();
+        const handleSubmit = () => {
+          // Save new comment to Firestore
+          if (comment !== "") {
+            addDoc(collection(docRef, "milestones", item.id, "comments"), {
+              user: user,
+              text: comment,
+              timestamp: serverTimestamp()
+            });
+            setComment("");
+          }
+        };
+
+        const [storedComments, setStoredComments] = useState([]);
+
+        const fetchCommentsHistory = async () => {
+          const q = query(
+            collection(docRef, "milestones", item.id, "comments"),
+            orderBy("timestamp", "asc")
+          );
+          const querySnapshot = await getDocs(q);
+          const newComments = [];
+          querySnapshot.forEach(comment => {
+            newComments.push({
+              text:comment.data().text,
+              user:comment.data().user
+            });
           });
+          setStoredComments(newComments);
+        };
+
+        const fetchNewComments = () => {
+          const q = query(
+            collection(docRef, "milestones", item.id, "comments"),
+            orderBy("timestamp", "asc")
+          );
+
+          const unsubscribe = onSnapshot(q, (snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+              if (change.type === "added") {
+                setStoredComments({
+                  text:change.doc.data().text,
+                  user:change.doc.data().user
+                });
+              }
+            });
+          });
+
+          return unsubscribe();
+        };
+
+        useEffect(() => {
+          fetchCommentsHistory();
+          // fetchNewComments();
+        }, []);
+
+        const LoadComments = ({ commentsArr }) => {
+          if (commentsArr === []) return null;
+          return (
+          <div>
+            <p><b>Comments</b></p>
+            {commentsArr.map((obj, index) => (
+              <p style={{align: "left"}} key={index}>
+                {obj.user}: {obj.text}
+                
+              </p>
+            ))}
+          </div>
+          );
         };
 
         return (
-          <Button size="small" variant="contained" onClick={handleDownload} color='primary'>
-            Download
-          </Button>
-        );
-      };
-
-      const Content = () => {
-        return (
-          <Grid container spacing={6} width="1000px" justifyContent="center" alignItems="flex-start">
-            <Grid item width="100px" sx={{ mr:5 }}>
-              <DownloadButton />
-            </Grid>
-            <Grid item width="400px" zeroMinWidth>
-              <p>{description}</p>
-            </Grid>
+        <Grid container spacing={2} sx={{ width:700, height:"100%" }}>
+          <Grid item xs={4} sx={{ mt:3 }}>
+            <Grid item><img style={{ maxWidth: "100%", height:"auto" }} src="https://www.swhf.sg/wp-content/uploads/2014/03/Halimah-Yacob.jpg"></img></Grid>
+            <Grid item sx={{ mb:1 }}><DownloadButton arr={urlArray} /></Grid>
+            <Grid item><Button variant="contained" onClick={handleSubmit}>Reply</Button></Grid>
           </Grid>
+          <Grid item xs={8}>
+            <p style={{alignSelf: "flex-start"}}>
+              {description}
+            </p>
+            <LoadComments commentsArr={storedComments} />
+            <TextField
+              label="Add Comment"
+              multiline
+              fullWidth
+              rows={4}
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </Grid>
+        </Grid>
         );
       };
 
       newItems.push({
         title: item.data().timestamp.toDate().toLocaleString(),
         cardTitle: item.data().subject,
-        // timelineContent: <div>
-        //   <p>Uploads</p>
-        //   <hr></hr>
-        //   {urlArray.map(url => <p><a href={url}>{url}</a></p>)}</div>,
         timelineContent: <Content />
-        
       });
     });
     setItems(newItems);
   };
   
   useEffect(() => {
-    fetchData();
+    fetchCardData();
   }, []);
 
+  // const cardRefs = useRef([]);
+
+  // const cardRefsCallback = (ref) => {
+  //   if (ref) {
+  //     cardRefs.current.push(ref);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // Calculate and set the height of each card dynamically
+  //   cardRefs.current.forEach((cardRef) => {
+  //     const contentHeight = cardRef.firstChild.offsetHeight;
+  //     cardRef.style.height = `${contentHeight}px`;
+  //   });
+  // }, [items]);
+
+  // console.log(cardRefs);
+
   return (
-    <div style={{ width: '1200px', height: '800px' }}>
+    <div style={{ width: '1200px', height: '850px' }}>
       <h1>Project {projectID}: Milestone {currentMilestone}</h1>
-      {items.length > 0 && <Chrono items={items} mode="VERTICAL" cardHeight="100" cardWidth="800" />}
+      <div className="card-wrapper">
+        {items.length > 0 && <Chrono
+          items={items}
+          mode="VERTICAL"
+          cardHeight="500"
+          />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TimelineTest;
