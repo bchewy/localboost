b74cded5f1c533226c4a5a74c65a718b00dc5c0b
Brian Chew <brian@bchewy.com>
2023-05-21 21:58:46 +0800

    Cleaned up code for timeline test since moving AI functions elsewhere

--- matts_pogwork/src/pages/TimelineTest.js
+++ matts_pogwork/src/pages/TimelineTest.js
@@ -1,49 +1,21 @@
 import React from "react";
-import { Grid, Button, TextField } from '@mui/material';
-
 import { useState, useEffect, useRef } from "react";
 import "../style.css";
 import "./styles.css";
 import { Chrono } from "react-chrono";
-import { Container, TextField, Button, LinearProgress, Typography, MenuItem, Select } from '@mui/material';
+import { Grid, Container, TextField, Button, LinearProgress, Typography, MenuItem, Select } from '@mui/material';
 import axios from 'axios';
 import { app } from './firebase';
 import { getFirestore, collection, getDocs, query, serverTimestamp, addDoc, orderBy, doc, where, onSnapshot } from "firebase/firestore";
-
 import DownloadButton from "../components/FileDownloadButton";
 
 // POPULATE WITH GPT
 
 const TimelineTest = (props) => {
 
-  const systemMessage = {
-    role: 'system', content: `
-  You are LocalBoost AI GPT, you only respond with an milestones array.
-  We want you to Generate and return ONLY the milestones array as a JSON response, for a {category} project.
 
-  const milestones = [{
-    title: "Name of the milestone",
-    cardTitle: "Milestone Title",
-    url: "milestone url, if any",
-    cardSubtitle:"Milestone subtitle",
-    cardDetailedText: "Milestone description",
-    media: {
-      type: "IMAGE",
-      source: {
-        url: "http://someurl/image.jpg"
-      }
-    }
-  }];
-  Only create 4 milestones.
-  Do not reply with anything else, but a JSON array with all the fields as shown above. Remember to close the JSON file.
-  ` };
-  const [messages, setMessages] = useState([systemMessage]);
-  const [aiResponse, setAiResponse] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [customOption, setCustomOption] = useState('');
 
   const [items, setItems] = useState([]);
-  const [selectedOption, setSelectedOption] = useState('');
   const user = "a0001";   // TODO: use authentication to check the current user uid
   const projectID = "gdbn3vGcDHgrsz3mbJin";
   const currentMilestone = 1;
@@ -196,115 +168,15 @@ const TimelineTest = (props) => {
   };
 
 
-
   useEffect(() => {
     fetchCardData();
   }, []);
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-
-  // useEffect(() => {
-  //   // Calculate and set the height of each card dynamically
-  //   cardRefs.current.forEach((cardRef) => {
-  //     const contentHeight = cardRef.firstChild.offsetHeight;
-  //     cardRef.style.height = `${contentHeight}px`;
-  //   });
-  // }, [items]);
-
-
-
-
-    try {
-      // console.log("Sending request to OpenAI API...");
-      // console.log("messages:", messages);
-      // console.log("system message:", messages[0].content);
-      // console.log("end of messages:");
-
-      const requestBody = {
-        model: 'gpt-3.5-turbo',
-        messages: [
-          ...messages,
-          { role: 'system', content: messages[0].content.replace('{category}', selectedOption) }
-        ],
-        max_tokens: 200,
-        // suffix: '}];', //Close the array
-        stream: true
-      };
-
-      const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        requestBody,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
-          },
-  
-        }
-      );
-
-
-      // Handle response data - is it half cut json data? or not?!
-      const responseData = new Uint8Array(response.data);
-      const decoder = new TextDecoder();
-      const responseText = decoder.decode(responseData);
-      
-      // Parse the complete JSON array from the responseText
-      const jsonArray = JSON.parse(responseText);
-      console.log('Complete JSON Array:', jsonArray);
-      
-      read().catch(error => {
-        console.error('Error occurred during stream:', error);
-      });
-
-      // console.log("Received response from OpenAI API: ", response);
-      // const aiMessage = { role: 'assistant', content: response.data.choices[0].message.content };
-      // const f_content = aiMessage.content
-      // console.log("f_content:", f_content)
-      // const parsedContent = JSON.parse(f_content);
-
-      // parsedContent.forEach((item) => {
-      //   console.log("Title:", item.title);
-      //   console.log("Card Title:", item.cardTitle);
-      //   console.log("URL:", item.url);
-      //   console.log("Card Subtitle:", item.cardSubtitle);
-      //   console.log("Card Detailed Text:", item.cardDetailedText);
-      //   console.log("Media Source URL:", item.media.source.url);
-      // });
-
-    } catch (error) {
-      console.error("Error occurred while calling OpenAI API: ", error);
-    } finally {
-      setIsLoading(false);
-    }
-
-
-  };
-
   return (
-    <div style={{ width: '1200px', height: '850px' }}>
-      <h3>Populate milestones with LocalBoost AI</h3>
-      <Typography variant="body1" gutterBottom> Settle on what you need for your project. Use Generative AI to help you come up with ideas. Edit after. </Typography>
-      {isLoading && <LinearProgress />}
-      <form onSubmit={handleSubmit}>
-        <Select
-          value={selectedOption}
-          onChange={handleOptionChange}
-          sx={{ width: '80%', height: '48px' }}
-        >
-          <MenuItem value="digitalisation">Digitalisation</MenuItem>
-          <MenuItem value="marketing">Marketing</MenuItem>
-          <MenuItem value="web development">Website Dev</MenuItem>
-          <MenuItem value="app development">Mobile App Dev</MenuItem>
-        </Select>
-        <Button type="submit" variant="contained" sx={{ width: '20%', height: '55px' }}>
-          Generate
-        </Button>
-      </form>
+    <div>
 
+      <div style={{ width: '1200px', height: '850px' }}>
       <h3>Milestones</h3>
       <h1>Project {projectID}: Milestone {currentMilestone}</h1>
       <div className="card-wrapper">
@@ -313,6 +185,7 @@ const TimelineTest = (props) => {
           mode="VERTICAL"
           cardHeight="500"
           />}
+      </div>
       </div>
     </div>
   );
