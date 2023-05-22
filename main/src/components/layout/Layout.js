0453bf51cef771f471bdb689e81a1ed7b44915de
Brian Chew <brian@bchewy.com>
2023-05-22 21:31:11 +0800

    ui changes

--- main/src/components/layout/Layout.js
+++ main/src/components/layout/Layout.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import classes from './Layout.module.css';
 import MainNavigation from './MainNavigation';
 
-function Layout(props){
+function Layout(props) {
+  const isUserSignedIn = true; // Replace this with your actual authentication logic
 
-    return <div>
-        <MainNavigation/>
-        <main className={classes.main}>
-            {props.children}
-        </main>
-    </div>;
-
+  return (
+    <div>
+      <MainNavigation isUserSignedIn={isUserSignedIn} />
+      <main className={classes.main}>
+        {props.children}
+      </main>
+    </div>
+  );
 }
 
 export default Layout;
