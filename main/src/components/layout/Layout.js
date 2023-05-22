cacc7e9977042ba445ef96c5eb16b1a59160e816
yijiyap <yijiyap@gmail.com>
2023-05-22 22:26:51 +0800

    Navbar now changes depending on if the user is logged in or not CREDITS GO TO BCHEWY

--- main/src/components/layout/Layout.js
+++ main/src/components/layout/Layout.js
@@ -1,15 +1,38 @@
+import React from 'react';
 import classes from './Layout.module.css';
 import MainNavigation from './MainNavigation';
+import { UserAuth } from '../auth/AuthContext';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState } from 'react';
 
-function Layout(props){
+function Layout(props) {
+      const [isUserSignedIn, setIsUserSignedIn] = useState(false); // Replace this with your actual authentication logic
+      const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsUserSignedIn(true);
+            } else {
+                setIsUserSignedIn(false);
+            }
+        });
 
-    return <div>
-        <MainNavigation/>
-        <main className={classes.main}>
-            {props.children}
-        </main>
-    </div>;
+    // const authContext = UserAuth();
+    // const isUserSignedIn = authContext.user !== undefined && authContext.user !== null;
+    // const isUserSignedIn = authContext.user && authContext.user.uid;
+        
+    // const { user } = UserAuth();
+    // const isUserSignedIn = !!user; // Check if user is defined and truthy
 
+    // const isUserSignedIn = user && Object.keys(user).length > 0;
+
+    return (
+        <div>
+      <MainNavigation isUserSignedIn={isUserSignedIn} />
+            <main className={classes.main}>
+                {props.children}
+            </main>
+        </div>
+    );
 }
 
 export default Layout;
